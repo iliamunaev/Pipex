66f4bb60b27c6e3a77d3f5d434c5744800653a1b
Ilia Munaev <ilyamunaev@gmail.com>
2024-12-11 09:38:00 +0200

    Refsctor: add functions descriptions

--- pipex.c
+++ pipex.c
@@ -6,12 +6,24 @@
 /*   By: imunaev- <imunaev-@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 14:18:01 by imunaev-          #+#    #+#             */
-/*   Updated: 2024/12/11 09:07:06 by imunaev-         ###   ########.fr       */
+/*   Updated: 2024/12/11 09:15:04 by imunaev-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
+/**
+ * @brief Executes a command by parsing the input string,
+ * locating the executable path, and invoking it with execve.
+ *
+ * @param av   The command string to execute (e.g., "ls -l").
+ * @param envp The environment variables.
+ *
+ * This function splits the command string into arguments,
+ * finds the full path to the executable, and then uses execve
+ * to execute the command. If any step fails, it
+ * handles the error appropriately.
+ */
 void	execute_command(char *av, char **envp)
 {
 	int		success;
@@ -37,6 +49,18 @@ void	execute_command(char *av, char **envp)
 		error();
 }
 
+/**
+ * @brief Handles the first child process in the pipex setup.
+ *
+ * @param fd      The file descriptors for the pipe.
+ * @param f_read  The file descriptor for reading input.
+ * @param av      The array of command-line arguments.
+ * @param envp    The environment variables.
+ *
+ * This function sets up the standard input and output
+ * for the first child process,
+ * closes unnecessary file descriptors, and executes the first command.
+ */
 void	child_1(int *fd, int f_read, char **av, char **envp)
 {
 	if (dup2(f_read, STDIN_FILENO) == -1)
@@ -47,6 +71,19 @@ void	child_1(int *fd, int f_read, char **av, char **envp)
 	execute_command(av[2], envp);
 }
 
+/**
+ * @brief Handles the second child process in the pipex setup.
+ *
+ * @param fd       The file descriptors for the pipe.
+ * @param f_write  The file descriptor for writing output.
+ * @param av       The array of command-line arguments.
+ * @param envp     The environment variables.
+ *
+ * This function sets up the standard input and output
+ * for the second child process,
+ * closes unnecessary file descriptors, and
+ * executes the second command.
+ */
 void	child_2(int *fd, int f_write, char **av, char **envp)
 {
 	if (dup2(fd[0], STDIN_FILENO) == -1)
@@ -57,6 +94,19 @@ void	child_2(int *fd, int f_write, char **av, char **envp)
 	execute_command(av[3], envp);
 }
 
+/**
+ * @brief Sets up the piping between two child processes to execute two commands.
+ *
+ * @param f_read  The file descriptor for reading input.
+ * @param f_write The file descriptor for writing output.
+ * @param av      The array of command-line arguments.
+ * @param envp    The environment variables.
+ *
+ * This function creates a pipe, forks two child processes, and
+ * assigns the appropriate
+ * standard input and output for each child.
+ * It then waits for both children to finish.
+ */
 void	pipex(int f_read, int f_write, char **av, char **envp)
 {
 	int		fd[2];
@@ -81,6 +131,20 @@ void	pipex(int f_read, int f_write, char **av, char **envp)
 	waitpid(child2, NULL, 0);
 }
 
+/**
+ * @brief The main entry point of the pipex program.
+ *
+ * @param ac    The argument count.
+ * @param av    The argument vector.
+ * @param envp  The environment variables.
+ *
+ * This function validates the command-line arguments,
+ * opens the input and output files, and initiates
+ * the piping process between the two commands.
+ *
+ * Usage:
+ *   ./pipex infile "cmd1" "cmd2" outfile
+ */
 int	main(int ac, char **av, char **envp)
 {
 	int	f_read;
--- utils.c
+++ utils.c
@@ -6,18 +6,37 @@
 /*   By: imunaev- <imunaev-@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 18:22:01 by imunaev-          #+#    #+#             */
-/*   Updated: 2024/12/11 09:07:20 by imunaev-         ###   ########.fr       */
+/*   Updated: 2024/12/11 09:27:38 by imunaev-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
+/**
+ * @brief Handles errors by printing an error message and exiting the program.
+ *
+ * This function uses `perror` to display the last error encountered during
+ * a system or library call. After displaying the error message, it terminates
+ * the program with a failure status.
+ *
+ * @return This function does not return as it exits the program.
+ */
 void	error(void)
 {
 	perror("Error");
 	exit(EXIT_FAILURE);
 }
 
+/**
+ * @brief Frees an array of strings allocated dynamically.
+ *
+ * This static helper function iterates through each string in the `paths` array,
+ * frees each individual string, and then frees the array itself.
+ *
+ * @param paths A null-terminated array of strings to be freed.
+ *
+ * @note This function should only be used internally within this source file.
+ */
 static void	free_paths(char **paths)
 {
 	int	i;
@@ -28,6 +47,24 @@ static void	free_paths(char **paths)
 	free(paths);
 }
 
+/**
+ * @brief Searches for the full path of a command within given directories.
+ *
+ * This static helper function iterates through each directory
+ * in the `paths` array, concatenates the directory with the command name
+ * to form a potential executable path, and checks if the file exists and
+ * is accessible.
+ * If a valid path is found, it frees the `paths` array and
+ * returns the full path to the executable. If no valid path is found,
+ * it frees the `paths` array and returns `NULL`.
+ *
+ * @param paths An array of directory paths to search within.
+ * @param cmd   The command name to locate.
+ *
+ * @return The full path to the executable if found; otherwise, `NULL`.
+ *
+ * @note This function should only be used internally within this source file.
+ */
 static char	*find_command_in_paths(char **paths, char *cmd)
 {
 	int		i;
@@ -51,6 +88,19 @@ static char	*find_command_in_paths(char **paths, char *cmd)
 	return (NULL);
 }
 
+/**
+ * @brief Retrieves the PATH environment variable and splits it
+ * into individual directories.
+ *
+ * This function scans the `envp` array for the `PATH` variable,
+ * extracts its value, and splits it into an array of
+ * directory paths using the `ft_split` function with ':' as the delimiter.
+ *
+ * @param envp The environment variables array.
+ *
+ * @return A dynamically allocated array of directory paths if
+ * `PATH` is found and split successfully; otherwise, `NULL`.
+ */
 char	**get_path_values(char **envp)
 {
 	char	**paths;
@@ -67,6 +117,21 @@ char	**get_path_values(char **envp)
 	return (paths);
 }
 
+/**
+ * @brief Locates the full path of a command by searching
+ * through PATH directories.
+ *
+ * This function retrieves the PATH directories using
+ * `get_path_values`, then uses `find_command_in_paths` to search
+ * for the command's executable within those directories.
+ * If the executable is found, it returns its full path.
+ * Otherwise, it returns `NULL`.
+ *
+ * @param cmd   The command name to locate (e.g., "ls").
+ * @param envp The environment variables array.
+ *
+ * @return The full path to the executable if found; otherwise, `NULL`.
+ */
 char	*get_path(char *cmd, char **envp)
 {
 	char	**paths;
